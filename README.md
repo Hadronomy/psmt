4bc2f66953522d59a6dce69d14eec1df750907a5
Pablo Hernández <noreply@hadronomy.com>
2022-01-05 22:17:36 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<div>
+  <h1 align=center>Dot</h1>
+  <p align=center>Pleeeease save my time :cry:</p>
+</div>
---
+++ src/enquirer/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Dot custom enquirer prompts
+ */
+
+import Enquirer from 'enquirer';
+
+const enquirer = new Enquirer();
+
+export default enquirer;
