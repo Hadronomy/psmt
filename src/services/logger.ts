5815cc16528560a5269755347de6d564e2a328f9
Pablo Hernández <hadronomy@gmail.com>
2022-01-18 10:46:43 +0000

    Added inline documentation

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,10 +1,18 @@
 import winston from 'winston';
 import { merge } from '@/utils';
 
+/**
+ * Returns the **default** `logger format`
+ * @param info
+ * @returns string
+ */
 function format(info : winston.Logform.TransformableInfo) : string {
   return `${info.label || ''}${info.level}: ${info.message}`;
 }
 
+/**
+ * The **default** `logger options`
+ */
 export const defaultOptions : winston.LoggerOptions = {
   level: 'info',
   format: winston.format.combine(
@@ -19,11 +27,23 @@ export const defaultOptions : winston.LoggerOptions = {
   ],
 };
 
+/**
+ * The `logger` that will be used by **default**
+ */
 export const defaultLogger : winston.Logger = winston.createLogger(defaultOptions);
 
+/**
+ * The **currently active** `logger`
+ */
 export const currentLogger : winston.Logger = defaultLogger;
 
-export function createLogger(options : winston.LoggerOptions) {
+/**
+ * Creates a new `logger` based on the `default options` and the pased `options`
+ * provided
+ * @param options
+ * @returns Logger
+ */
+export function createLogger(options : winston.LoggerOptions) : winston.Logger {
   return winston.createLogger(merge(defaultOptions, options));
 }
 
